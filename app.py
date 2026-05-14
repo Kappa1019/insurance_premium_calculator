99cb3bda56840405c84efb899b04fdb6fd36bb54
Kappa1019 <mishradevansh2005@gmail.com>
2026-05-14 17:05:40 +0530

    first

---
+++ app.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from schema.user_input import UserInput
+from model.predict import predict_output, model, MODEL_VERSION
+
+app = FastAPI()
+
+# human readable
+@app.get('/')
+def home():
+    return{'message':'Insurance Premimum Precdictio API'}
+
+# machine readable
+@app.get('/health')
+def health_check():
+    return{
+        'status':'OK',
+        'version': MODEL_VERSION,
+        'model_loaded': model is not True            
+
+    }
+
+@app.post('/predict')
+def predict_premium(data: UserInput):
+
+    user_input = ([{
+        'bmi': data.bmi,
+        'age_group': data.age_group,
+        'lifestyle_risk': data.lifestyle_risk,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation
+    }])
+
+    prediction = predict_output()
+
+    return JSONResponse(status_code=200, content={'predicted_category': prediction})
+
+
+
+
